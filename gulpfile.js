5b8ad4872022adf0329a69a1f47dcaa18ea1ebd6
Goloveshkin Nikolay <kolya-mp19@mail.ru>
2019-03-19 21:27:04 +0300

    guip

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,89 @@
-﻿//Подключаем локальные пакеты
-var gulp = require("gulp");
-var sass = require("gulp-sass");
+//Подключаем модули галпа
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const del = require('del');
+const browserSync = require('browser-sync').create();
 
-//Определение задачи по умолчанию в GULP
-gulp.task("default", function () {
-    console.log(10);
-})
+//Порядок подключения css файлов
+const cssFiles = [
+   './src/css/main.css',
+   './src/css/media.css'
+]
+//Порядок подключения js файлов
+const jsFiles = [
+   './src/js/lib.js',
+   './src/js/main.js'
+]
 
-//Определение новой задачи в GULP для SCSS и SASS 
-gulp.task("scss", function () {
-    //**- ищем везде, включая вложенные папки
-    gulp.src(["./samples/**/*.scss", "./samples/**/*.sass"])
-        //Компиляция scss или sass в css формат
-    .pipe(sass())
-        //Вывод ошибки, без окончания компиляции
-  .pipe(sass().on("error", sass.logError))
-        //Путь компиляции файлов
-.pipe(gulp.dest("./samples"))
-})
+//Таск на стили CSS
+function styles() {
+   //Шаблон для поиска файлов CSS
+   //Всей файлы по шаблону './src/css/**/*.css'
+   return gulp.src(cssFiles)
+   //Объединение файлов в один
+   .pipe(concat('style.css'))
+   //Добавить префиксы
+   .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+   }))
+   //Минификация CSS
+   .pipe(cleanCSS({
+      level: 2
+   }))
+   //Выходная папка для стилей
+   .pipe(gulp.dest('./build/css'))
+   .pipe(browserSync.stream());
+}
 
-//Отслеживание изменений
-gulp.task("scss:watch", function () {
-    //Находим папку и отмечаем, за каким файлом необходимо наблюдать, если он найден - выполняем задание "scss"
-    gulp.watch(["./samples/**/*.scss", "./samples/**/*.sass"], ["scss"])
-})
+//Таск на скрипты JS
+function scripts() {
+   //Шаблон для поиска файлов JS
+   //Всей файлы по шаблону './src/js/**/*.js'
+   return gulp.src(jsFiles)
+   //Объединение файлов в один
+   .pipe(concat('script.js'))
+   //Минификация JS
+   .pipe(uglify({
+      toplevel: true
+   }))
+   //Выходная папка для скриптов
+   .pipe(gulp.dest('./build/js'))
+   .pipe(browserSync.stream());
+}
+
+//Удалить всё в указанной папке
+function clean() {
+   return del(['build/*'])
+}
+
+//Просматривать файлы
+function watch() {
+   browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
+  //Следить за CSS файлами
+  gulp.watch('./src/css/**/*.css', styles)
+  //Следить за JS файлами
+  gulp.watch('./src/js/**/*.js', scripts)
+  //При изменении HTML запустить синхронизацию
+  gulp.watch("./*.html").on('change', browserSync.reload);
+}
+
+//Таск вызывающий функцию styles
+gulp.task('styles', styles);
+//Таск вызывающий функцию scripts
+gulp.task('scripts', scripts);
+//Таск для очистки папки build
+gulp.task('del', clean);
+//Таск для отслеживания изменений
+gulp.task('watch', watch);
+//Таск для удаления файлов в папке build и запуск styles и scripts
+gulp.task('build', gulp.series(clean, gulp.parallel(styles,scripts)));
+//Таск запускает таск build и watch последовательно
+gulp.task('dev', gulp.series('build','watch'));
